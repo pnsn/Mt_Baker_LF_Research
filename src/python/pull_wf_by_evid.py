b9be92d5ae07b78f5bcba1155548b59c45e101a1
Benz Poobua <supakorn1207@gmail.com>
2024-10-09 14:24:29 -0700

    Add pull-waveform script

--- src/python/pull_wf_by_evid.py
+++ src/python/pull_wf_by_evid.py
@@ -4,9 +4,20 @@
 :email: spoobu (at) uw.edu
 :org: University of Washington
 :license: GNU GPLv3
-:purpose: 
+:purpose: This module is used to fetch events from AQMS and fetch subset waveform pickings from IRIS web services.
 
 """
+import os,sys
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np 
+
+from obspy import read
+from obspy.core import UTCDateTime
+from obspy.clients.fdsn import Client
+
+sys.path.append(os.path.join('..', 'python'))
+from util import unix_to_UTCDateTime
 
 def format_aqms_csv(filename, colmapping={'datetime': 'origin_datetime', 'datetime.1': 'arrival_datetime'}):
     """This method reads a raw output CSV file from an AQMS file, renames columns 
@@ -22,48 +33,120 @@ def format_aqms_csv(filename, colmapping={'datetime': 'origin_datetime', 'dateti
      - **df** (*pandas.dataframe.DataFrame*) -- loaded, formatted
     """    
 
+    rrpath = os.path.join('..', '..')
+    phases = os.path.join(rrpath, 'data', 'Events', filename)
+    phases_df = pd.read_csv(phases)
+    phases_df.rename(columns=colmapping, inplace=True)
+    phases_df.origin_datetime = phases_df.origin_datetime.apply(lambda x: unix_to_UTCDateTime(x))
+    phases_df.arrival_datetime = phases_df.arrival_datetime.apply(lambda x: unix_to_UTCDateTime(x))
+
+    # for col in ['fdepth', 'fepi', 'ftime']:
+    #     if col in phases_df.columns:
+    #         phases_df[col] = phases_df[col].apply(lambda x: x == 'y')
+    
+    return phases_df
+
 def get_phase_entries(dataframe, evid):
     """Get a subset of phase pick rows from input **dataframe** that match the specified **evid**
 
-    :param dataframe: _description_
-    :type dataframe: _type_
-    :param evid: _description_
-    :type evid: _type_
+    :param dataframe: The DataFrame containing phase pick data.
+    :type dataframe: pandas.DataFrame
+    :param evid: The event ID to filter by.
+    :type evid: int
+    :return: A DataFrame containing rows that match the specified evid.
+    :rtype: pandas.DataFrame
     """    
+    if 'evid' not in dataframe.columns:
+        raise ValueError("The DataFrame does not contain an 'evid' column.")
 
-def get_waveforms_from_phases(dataframe, client, front_pad_sec=30, back_pad_sec=60):
-    """Fetch waveform data from IRIS web services for picks provided in **dataframe**
+    filtered_entries = dataframe[dataframe.evid == evid]
+    return filtered_entries
+
+def get_waveforms_from_phases(client, pick, front_pad_sec=30, back_pad_sec=60):
+    """Fetch waveform data from IRIS web services for the provided pick
     and pad pick times by specified seconds before (front) & after (back).
 
-    Make sure the function dosen't terminate if it hits a No-Data-Available type exception.
+    The function will not terminate if it encounters a No-Data-Available exception.
 
-    :param dataframe: _description_
-    :type dataframe: _type_
-    :param client: _description_
-    :type client: _type_
-    :param front_pad_sec: _description_, defaults to 30
+    :param client: The IRIS web service client.
+    :type client: str
+    :param pick: The specific pick entry to fetch waveforms for.
+    :type pick: pandas.Series
+    :param front_pad_sec: Seconds to pad before the pick, defaults to 30.
     :type front_pad_sec: int, optional
-    :param back_pad_sec: _description_, defaults to 60
+    :param back_pad_sec: Seconds to pad after the pick, defaults to 60.
     :type back_pad_sec: int, optional
     """
 
+    # # Get the phase entry for the specified evid
+    # lf_baker = get_phase_entries(dataframe, dataframe['evid'])
 
-def get_waveforms(filename, evid, savepath='.'):
-    """Composite function of the above.
+    # # Filter the pick based on station
+    # pick = lf_baker[lf_baker.sta == 'MBW'].iloc[0, :] if not lf_baker.empty else None
 
-    :param filename: _description_
-    :type filename: _type_
-    :param evid: _description_
-    :type evid: _type_
-    :param savepath: _description_, defaults to '.'
+    if pick is not None:
+        client_ = Client(client)
+        time_ = UTCDateTime(pick['arrival_datetime'])
+        starttime = time_ - front_pad_sec
+        endtime = time_ + back_pad_sec
+        net = pick.net
+        sta = pick.sta
+        loc = pick.location
+        chan = pick.seedchan
+
+        try:
+            Z = client_.get_waveforms(net,sta,loc,chan, starttime, endtime, attach_response=True)
+            Z.remove_response(output='VEL')
+            Z.merge()
+            Z.detrend(type='linear')
+            Z.taper(max_percentage=0.05)
+            #Z.filter('bandpass',freqmin=0.5, freqmax=25)
+            Z.plot();
+            return Z
+        except Exception as e:
+            print(f"Error fetching waveforms: {e}")
+            return None
+
+    else: 
+        print("No valid pick found for the specified evid.")
+        return None
+
+def get_waveforms(filename, evid, client, pick, savepath='../../results/waveforms'):
+    """Composite function to get waveforms based on a CSV file and event ID.
+
+    :param filename: Name of the CSV file to load phase picks.
+    :type filename: str
+    :param evid: Event ID to filter phase picks.
+    :type evid: int or str
+    :param client: The IRIS web service client.
+    :type client: str
+    :param savepath: Directory to save the waveform file, defaults to '../../results/waveforms'.
     :type savepath: str, optional
-    :return: _description_
-    :rtype: _type_
+    :return: The Stream object containing waveforms.
+    :rtype: obspy.Stream
     """
+    
+    # Ensure the output directory exists
+    if not os.path.exists(savepath):
+        os.makedirs(savepath)
 
+    # Load and filter phases
+    phases_df = format_aqms_csv(filename)
+    filtered_events = get_phase_entries(phases_df, evid)
 
+    # Get waveforms
+    st = get_waveforms_from_phases(client, pick)
 
-    out_file_name = f'uw{evid}.mseed'
-    save_name = os.path.join(savepath, out_file_name)
-    st.write(out_file_name)
+    # Construct the output filename
+    if st is not None and len(st) > 0:
+        out_file_name = f'{st[0].id}.{evid}.mseed'
+        save_name = os.path.join(savepath, out_file_name)
+
+        # Save the waveforms
+        st.write(save_name, format='MSEED')
+        print(f'Waveforms saved to {save_name}')
+    else:
+        print('No waveforms to save.')
+
     return st
+
