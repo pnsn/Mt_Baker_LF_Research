54dac43c98931b8455327a27bfec7c4640ce1ee0
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-10-16 12:15:17 -0700

    Add wildcard event_id search

--- src/python/classes/eventbank2.py
+++ src/python/classes/eventbank2.py
@@ -10,9 +10,35 @@
     see: https://github.com/niosh-mining/obsplus
 
 """
+import fnmatch
+import obspy
+import pandas as pd
 from obsplus import EventBank
 from obsplus.bank.eventbank import compose_docstring
 from obsplus.constants import get_events_parameters
+
+def _parse_event_fix_status(event: obspy.core.event.Event) -> dict:
+    """Parse the fixed solution status of an :class:`~obspy.core.event.Event` object
+    and return a dictionary formatted with standard AQMS fields plus 'event_id'
+    
+    :param event: Event object to parse
+    :type event: obspy.core.event.Event
+    :return: 
+     - **out** (*dict*) - dictionary with 
+    """    
+    event_id = str(event.resource_id)
+    try:
+        origin = event.preferred_origin()
+    except AttributeError:
+        origin = event.origins[0]
+    if origin.depth_type == 'from location':
+        fdepth = False
+    else:
+        fdepth = True
+    ftime = origin.time_fixed
+    fepi = origin.epicenter_fixed
+    out = {'event_id': event_id,'fepi': fepi,'fdepth':fdepth,'ftime': ftime}
+    return out
 
 
 class EventBank2(EventBank):
@@ -47,13 +73,52 @@ class EventBank2(EventBank):
         ----------
         {get_events_params}
         """
+        # Restrict access to COMCAT_ID as a search parameter & provide hint
+        if 'COMCAT_ID' in options:
+            raise ValueError('use `event_id` column entries to fetch subset events by EVID')
+        # If using new kwarg, pop off this kwarg from options to allow for expected inputs
+        # to original read_index() method. If not using this, set internal var to False
+        if 'include_fixed_status' in options.keys():
+            ifs = options.pop('include_fixed_status')
+        else:
+            ifs = False
+
+        # Enable wildcard search for event_id
+        # If event_id is in **options
+        if 'event_id' in options:
+            # If event_id's value is a string
+            if isinstance(options['event_id'], str):
+                # if that string has wildcard character(s) present
+                if '?' in options['event_id'] or '*' in options['event_id']:
+                    # pop the event_id entry off options
+                    wild_entry = options.pop('event_id')
+                    # run everyting else as normal
+                    df = super().read_index(**options)
+                    event_id=fnmatch.filter(df.event_id.values, wild_entry)
+                    options.update({'event_id': event_id})
+
         # Run as normal
         df = super().read_index(**options)
-        # Update index
+
+        # Handle fetching fixed status entries
+        if ifs:
+
+            fentries = []
+            for eid in df.event_id:
+                event = self.get_events(event_id=eid)[0]
+                # Aggregate
+                fentries.append(_parse_event_fix_status(event))
+            # Create dataframe
+            fentries = pd.DataFrame(fentries)
+            # Merge dataframe
+            df = pd.merge(df, fentries, on='event_id', how='left')
+                    
+        # Update index with COMCAT_ID
         df.index = [row.agency_id.lower() + row.event_id.split('/')[-1] for _, row in df.iterrows()]
         # Update index name
         df.index.name = 'COMCAT_ID'
+                    
         return df
     
-    
+
     
