bf05094af77c7fca11b78a10cff50d9af06b3001
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2024-11-13 08:54:45 -0800

    WIP for MC NMI objective function

--- src/python/drivers/postprocess_mbs_clusters.py
+++ src/python/drivers/postprocess_mbs_clusters.py
@@ -26,12 +26,86 @@ from eqcutil.util.logging import setup_terminal_logger
 
 
 ### PARAMETER SETTING SECTION ###
+# Seed random number generator
+np.random.seed(62323)
 
-kwargs = {'n_depth_bins': 20,
+kwargs = {'n_depth_bins': 4,
           'average_method': 'arithmetic',
-          'etype_wgt': 4}
+          'etype_wgt': 10}
 
 cct_range = np.arange(0.005, 1., 0.005)
+
+### MONTE CARLO SIMULATION SAMPLER
+def generate_perturbed_value_bins(locs, scales, nbins=4, distribution='normal'):
+    """Given the expected values (locs) and characteristic scales of a vector
+    of data, generate perturbed data samples for a prescribed distribution type
+    and bin them into equal sample-count bins
+
+    :param locs: expected parameter values
+    :type locs: vector-like
+    :param scales: characteristic scales of parameter uncertainties (see **distribution**)
+    :type scales: vector-like
+    :param nbins: number of equal-sample scaled bins to produce, defaults to 4
+        see :meth:`~pandas.qcut` - passed to parameter `q`
+    :type nbins: int, optional
+    :param distribution: name of the distribution to use, defaults to 'normal'
+        Supported values:
+            'normal' - locs are the means, scales are the standard deviations
+            'uniform' - min is locs - scales, max is locs + scales
+    :type distribution: str, optional
+    :return:
+     - **groups** (*pandas.Categorical*) - unlabeled bin designations
+        of perturbed MC sample
+    """    
+    if distribution.lower() == 'normal':
+        pert_vals = np.random.normal(loc=locs, scale=scales)
+    elif distribution.lower() == 'uniform':
+        pert_vals = np.random.uniform(low=locs - scales, high=locs + scales)
+    else:
+        raise ValueError(f'distribution {distribution} not supported')
+    groups = pd.qcut(pert_vals, q=nbins, labels=False)
+    return groups
+        
+def evaluate_score_mc(ref_grouping, locs, scales, nbins=4, ndraw=30, 
+                      distribution='normal', average_method='arithmetic'):
+    """Conduct a Monte Carlo simulation to assess the normalized mutual information
+    score between a reference grouping and a vector of random variables defined by
+    data values, uncertainties, and assumed underlying distribution. 
+
+    :param ref_grouping: reference grouping
+    :type ref_grouping: vector-like
+    :param locs: expected values of random variable vector members
+    :type locs: vector-like
+    :param scales: uncertainty/range of random variable vector members
+    :type scales: vector-like
+    :param nbins: number of even-sample-count bins to group the random 
+        variable by, defaults to 4
+    :type nbins: int, optional
+    :param ndraw: number of sample sets to draw for this simulation, 
+        defaults to 30
+    :type ndraw: int, optional
+    :param distribution: name of the distribution to use, defaults to 'normal'
+        Supported: 'normal','uniform'
+    :type distribution: str, optional
+    :param averaging_method: averaging method for the NMI score, defaults to 'arithmetic'
+    :type averaging_method: str, optional
+    :return: 
+     - **scores** (*numpy.ndarray*) -- vector of scores from each MC simulation draw
+    """    
+    scores = []
+    for _n in ndraw:
+        sample = generate_perturbed_value_bins(locs, scales, nbins=nbins, distribution=distribution)
+        score = normalized_mutual_info_score(ref_grouping, sample, average_method=average_method)
+        scores.append(score)
+    return np.array(scores)
+
+def objective(ctr, cct, n_depth_bins=4, n_depth_draw=30, average_method='arithmetic', etype_wgt=2.):
+    acdf = ctr.clusters
+    locs = acdf.depth.values
+    scales = acdf.vertical_uncertainty.values
+    dscores = evaluate_
+
+
 
 ### EVALUATION FUNCTION ###
 def evaluate_corr_thresh(clustertribe, threshold, n_depth_bins=8, average_method='arithmetic', etype_wgt=2.):
@@ -43,11 +117,15 @@ def evaluate_corr_thresh(clustertribe, threshold, n_depth_bins=8, average_method
     df_c = pd.concat([clusters, clustertribe.clusters[['etype','depth']]], axis=1, ignore_index=False)
     # Get cluster values
     cvals = df_c['correlation_cluster'].values
-    escore = normalized_mutual_info_score(df_c.etype.values, cvals, average_method=average_method)
-    dscore = normalized_mutual_info_score(pd.qcut(df_c.depth.values, n_depth_bins, labels=False), cvals, average_method=average_method)
+    escore = normalized_mutual_info_score(cvals, df_c.etype.values, average_method=average_method)
+    dscore = normalized_mutual_info_score(cvals, pd.qcut(df_c.depth.values, n_depth_bins, labels=False), average_method=average_method)
 
     return (escore*etype_wgt + dscore)/(etype_wgt + 1.)
 
+def perturb_depths(clustertribe):
+
+
+def evaluate_nmi()
 
 
 # Setup Logging
@@ -106,6 +184,7 @@ idx = [eid.split('/') for eid in df_eb.event_id]
 idx = [f'{eid[-2].lower()}{eid[-1]}' for eid in idx]
 df_eb.index = idx
 
+bests = {}
 results = {}
 # Iterate across clustering trials
 for filepath in ctr_files:
@@ -119,17 +198,18 @@ for filepath in ctr_files:
     ctr.clusters = ctr.clusters.join(df_etype, how='left')
     # Join eventbank metadata
     ctr.clusters = ctr.clusters.join(df_eb, how='left')
-    Logger.info(f'Running scoring for {len(cct_range)} cross_correlations')
+    Logger.info(f'Running scoring for {len(cct_range)} cross_correlation threshold values')
     scores = []
     for _e, thresh in enumerate(cct_range):
         Logger.debug(f'{_e+1} of {len(cct_range)}')
         scores.append(evaluate_corr_thresh(ctr, thresh, **kwargs))
-    
+    results.update({filename: scores})
     best_thresh = cct_range[np.argmax(scores)]
+    bests.update({filename: (best_thresh, np.max(scores))})
 
     Logger.info(f'{filename}: {best_thresh} ({np.max(scores)})')
-    results.update({filename: (best_thresh, np.max(scores))})
 
+results = pd.DataFrame(results, index=cct_range)
 breakpoint()
     
 
