e009c2a59707f79f7f6e9fb11f2e45f6d4da3a01
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2025-01-10 14:59:16 -0800

    Get new events from AQMS

---
+++ src/PostgreSQL/PNSN_MB_evid_query.sql
@@ -0,0 +1,13 @@
+\copy SELECT e.evid
+FROM event e
+    INNER JOIN origin o ON e.prefor = o.orid
+WHERE (
+    6371. * acos(
+        cos(radians(48.7745)) * cos(radians(o.lat)) *
+        cos(radians(o.lon) - radians(-121.8172)) + 
+        sin(radians(48.7745)) * sin(radians(o.lat))
+        )
+    ) <= 50.
+    AND e.selectflag = 1
+    AND o.datetime > 1727959393
+ORDER BY o.datetime TO "MtBaker_50km_evids_supp.csv" CSV HEADER;
