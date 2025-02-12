ed6f3344436c2c3819b14e9ee1d2ee83140b5464
Nate Stevens <33462727+nts345045@users.noreply.github.com>
2025-02-11 22:05:34 -0800

    Rebuild event augmentation script

--- src/phase1_catalog_curation/step3_prep_template_metadata.py
+++ src/phase1_catalog_curation/step3_prep_template_metadata.py
@@ -38,12 +38,13 @@ SHIFTS = {'N': 'Z',
 
 # Pick Modeling Parameters
 MODPHZ = ['P','p']
-VMOD = 'P5'
+VMOD = 'P4'
 MODCHAN = '??Z'
 ITYPE_PRIORITY = ['HH','BH','EH','HN','EN']
 
 
 def main():
+    # Make sure augmented bank base path directory exists
     try:
         os.makedirs(AEBBP)
     except FileExistsError:
@@ -67,6 +68,8 @@ def main():
         for _f in ['network','station','channel']:
             unique_nslc[_f].add(row[_f])
 
+    STAS = unique_nslc['station']
+    # Compile key-word arguments for Inventory query
     qkwargs = {_k: ','.join(list(_v)) for _k, _v in unique_nslc.items()}
     qkwargs.update({'longitude': LON_REF,
                     'latitude': LAT_REF,
@@ -75,16 +78,12 @@ def main():
     # Query webservice for metadata
     INV = IRIS.get_stations(**qkwargs)
 
-    # Get unique event_ids
-    pref_evid = df_pref.event_id.unique()
-    for evid in pref_evid:
+    for event_id, etype in df_pref[['event_id','etype']].value_counts().index:
         # Load event
-        cat = EBANK.get_events(event_id=evid)
+        cat = EBANK.get_events(event_id=event_id)
         # Ensure event is non-empty
         if len(cat) != 1:
             Logger.critical(f'fetched catalog with {len(cat)} events')
-        else:
-            event = cat[0]
 
         # Apply phase hints to Pick objects
         cat = apply_phase_hints(cat)
@@ -94,8 +93,133 @@ def main():
             phase_hints=MODPHZ,
             enforce_single_pick='preferred',
             stations=list(unique_nslc['station']))
-        breakpoint()
+        
+        event = cat[0]
+        # Get picked channels and apply channel shifts
+        picksta = set()
+        for _p in event.picks:
+            _sta = _p.waveform_id.station_code
+            _cha = _p.waveform_id.channel_code
+            if _cha[-1] in SHIFTS.keys():
+                newcode = _cha[:-1] + SHIFTS[_cha[-1]]
+                _p.waveform_id.channel_code = newcode
+                Logger.warning(f'Shifted {_p.waveform_id.id} channel to {newcode}')
+            picksta.add(_sta)
 
+        # Insert ETYPE as comment on event
+        event.comments.append(Comment(text=etype))
+        # Get metadata from retained picks & apply channel shifts
+        picksta = set([])
+        kprid = []
+        Logger.debug(f'Filtering preserved {len(event.picks)} picks')
+        for _p in event.picks:
+            kprid.append(_p.resource_id)
+            picksta.add(_p.waveform_id.station_code)
+            ccode = _p.waveform_id.channel_code
+            # Apply component shifts as needed
+            if ccode[-1] in SHIFTS.keys():
+                scode = ccode[:-1] + SHIFTS[ccode[-1]]
+                comment = Comment(text=f'Originally on channel {ccode}')
+                Logger.warning(f'Shifting pick on {_p.waveform_id.id} to component {scode}')
+                _p.waveform_id.channel_code = scode
+                _p.comments.append(comment)
+                
+        # Station magnitudes cleanup
+        smk = []
+        for smag in event.station_magnitudes:
+            if smag.waveform_id.station_code in STAS:
+                smk.append(smag)
+        Logger.debug(f'Removing {len(event.station_magnitudes) - len(smk)} unreferenced station_magnitudes')
+
+        event.station_magnitudes = smk
+
+        # Amplitudes cleanup
+        amk = []
+        for amp in event.amplitudes:
+            if amp.waveform_id.station_code in STAS:
+                amk.append(amp)
+        Logger.debug(f'Removing {len(event.amplitudes) - len(amk)} unreferenced amplitudes')
+        event.amplitudes = amk
+        prefor = event.preferred_origin()
+
+        # Arrivals cleanup
+        ark = []
+        for arr in prefor.arrivals:
+            if arr.pick_id in kprid:
+                ark.append(arr)
+        Logger.debug(f'Removing {len(prefor.arrivals) - len(ark)} unreferenced arrivals')
+        prefor.arrivals = ark
+
+        # MODEL ARRIVAL TIMES
+        # Get active station list
+        inv = INV.select(time=prefor.time, channel=MODCHAN)
+        active = inv.get_contents()['channels']
+        astas = set([_c.split('.')[1] for _c in active])
+        # Get stations to model for
+        modsta = list(set(astas).difference(picksta))
+        # Announce stations being modeled
+        msg = 'Modelling arrival times for station(s):'
+        for ms in modsta:
+            msg += f' {ms},'
+        msg = msg[:-1]
+        Logger.debug(msg)
+
+        modeled_first_arrivals = []
+        # Iterate across each active station that is not picked
+        for sta in modsta:
+            # Get the subset inventory for that station
+            sinv = inv.select(station=sta)
+            # Iterate across instrument/band priorities
+            for itype in ITYPE_PRIORITY:
+                # Check sub-sub-inventory
+                sbinv = sinv.select(channel=f'{itype}?')
+                # If we find a component that has one represented channel
+                # Break and continue to modeling
+                if len(sbinv.get_contents()['channels']) == 1:
+                    break
+            # If we've gone across all instrument types and get nothing
+            if len(sbinv.get_contents()['channels']) != 1:
+                # Continue to next station
+                continue
+            
+            # Model picks
+            picks_hat = model_picks(
+                prefor,
+                sbinv,
+                model_name=VMOD,
+                phases=MODPHZ
+            )
+            # Take earliest pick
+            t0 = UTCDateTime()
+            for _ph in picks_hat:
+                if _ph.time < t0:
+                    t0 = _ph.time
+                    earliest = _ph
+
+            if len(picks_hat) > 0:
+                modeled_first_arrivals.append(earliest)
+            else:
+                continue
+    
+        Logger.info(f'Adding {len(modeled_first_arrivals)} modeled first arrivals to {event_id}')
+        # Append modeled picks to event (should go back to cat)
+        pre_append_phz = len(event.picks)
+        event.picks += modeled_first_arrivals
+        if len(cat[0].picks) != pre_append_phz + len(modeled_first_arrivals):
+            breakpoint()
+        # try:
+        ABANK.put_events(cat)
+        Logger.info('Successfully added to Augmented Event Bank')
+        # except:
+            # breakpoint()   
+
+
+    # for _p in event.picks:
+    #     nslc = _p.waveform_id.id
+    #     line = [event_id, _p.phase_hint, nslc, _p.evaluation_mode] + nslc.split('.')
+    #     pick_info.append(line)
+    # Try committing updated event to AUGMENTED EVENT BANK
+    
 if __name__ == '__main__':
     # Setup Logging
     Logger = setup_terminal_logger(os.path.split(__file__)[-1], level=logging.DEBUG)
