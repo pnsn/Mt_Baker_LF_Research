d6d42bc803e1da8a86b3e4ddf569157167ad33c1
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2025-02-13 08:56:37 -0800

    Cleanup for operation

--- src/phase2_template_construction/step2_assemble_clusters.py
+++ src/phase2_template_construction/step2_assemble_clusters.py
@@ -12,14 +12,15 @@ TDIR = ROOT / 'processed_data' / 'template' / 'single_station'
 # Absolute path to results subdirectory
 RDIR = ROOT / 'results' / 'cluster' / 'single_station'
 
+WRITE_PROTECT = False
 ALIAS_INSTRUMENTS = True
+
 ccckwargs = {'method': 'xcc',
             'replace_nan_distances_with': 'mean',
             'shift_len': 10,
             'corr_thresh': 0.5,
             'allow_individual_trace_shifts': True,
             'cores': 'all'}
-
 
 def main():
     # Ensure main save directory exists
@@ -29,86 +30,66 @@ def main():
     chan_dirs = glob.glob(str(TDIR/'*'))
     chanids = [_c.split('/')[-1] for _c in chan_dirs]
     chanids.sort()
-    
-    
-
-    processed_chanids = set()
-
+    # Iterate across channel IDs
+    processed = set()
     for chanid in chanids:
+        mask_loc = False
+        mask_bi = False
         n,s,l,c = chanid.split('.')
         # Group multiple location codes
         if len(glob.glob(str(TDIR/f'{n}.{s}.*.{c}'))) > 1:
             chanid = f'{n}.{s}.*.{c}'
+            name = f'{n}.{s}.XX.{c}'
+            mask_loc = True
         # Group multiple sensors/sampling bands
         elif len(glob.glob(str(TDIR/f'{n}.{s}.{l}.*'))) > 1:
             chanid = f'{n}.{s}.{l}.??{c[-1]}'
+            name = f'{n}.{s}.{l}.XX{c[-1]}'
+            mask_bi = True
         else:
-            pass
+            name = chanid
+        if name in processed:
+            continue
+        # ACTUALLY get file list
+        flist = glob.glob(str(TDIR/chanid/'*'/'*'/'*.tgz'))
+        # Make savename
+        SAVEFP = RDIR / f'{name}.tgz'
+        if SAVEFP.is_file() and WRITE_PROTECT:
+            continue
         
-
-
-        # Skip location 01's that will 
-        if chanid.split('.')[2] == '01':
-            Logger.warning(f'Skipping {chanid} - has 01 location code that will be merged with -- location code')
-            continue
-        Logger.info(f'Processing {chanid}')
-        n,s,l,c = chanid.split('.')
-        clist = glob.glob(str(TDIR/f'{n}.{s}.{l}.*'))
-        # Check if there are Band / Instrument codes to alias
-        if len(clist) > 1 and ALIAS_INSTRUMENTS:
-            bi = 'XX'
-            Logger.warning('Mixed instruments found, will alias band & instrument codes on traces to XX')
-            chanid = f'{n}.{s}.{l}.{bi}{c[-1]}'
-        else:
-            bi = False
-        llist = glob.glob(str(TDIR/f'{n}.{s}.*.*'))
-        if len(llist) > 1:
-
-        if chanid in processed_chanids:
-            Logger.warning(f'CHANID {chanid} already processed - skipping')
-            continue
-        tlist = glob.glob(str(TDIR/f'{n}.{s}.*.*'/'*'/'*'/'*.tgz'))
-        SAVEFP = RDIR / f'{chanid}.tgz'
-        if SAVEFP.is_file():
-            continue
         ctr = ClusteringTribe()
-        for _e, _t in enumerate(tlist):
-            Logger.debug(f'Loading {_t}')
+        for _e, _f in enumerate(flist):
             # Update every 10
             if _e %10 == 0 and _e > 0:
-                Logger.info(f'({_e+1}/{len(tlist)})')
-            # Load Template
-            template = Tribe().read(_t)[0]
-            # If band/instrument are aliased
-            if bi:
-                # "Iterate" across trace
+                Logger.info(f'{name} - ({_e+1}/{len(flist)})')
+            if mask_loc:
+                Logger.debug(f'{name} - ALIASED LOCATIONS')
+            if mask_bi:
+                Logger.debug(f'{name} - ALIASED BAND AND INSTRUMENT CODES')
+            Logger.debug(f'Loading {_f}')
+            template = Tribe().read(_f)[0]
+            if mask_loc or mask_bi:
                 for tr in template.st:
-                    Logger.warning(f'Alias {tr.stats.channel} --> {bi}{tr.stats.component}')
-                    tr.stats.channel = f'{bi}{tr.stats.component}'
-            # add to clustering tribe
+                    if mask_loc:
+                        Logger.debug(f'aliasing {tr.id} location')
+                        tr.stats.location = 'XX'
+                    if mask_bi:
+                        Logger.debug(f'aliasing {tr.id} band and instrument codes')
+                        tr.stats.channel = f'XX{tr.stats.component}'
             ctr += template
-        # Apply alias to clustering tribe save name
-
-        # Run clustering
-        Logger.warning(f'Running clustering for {chanid}')
+        Logger.info(f'RUNNING CLUSTERING FOR {name}')
         ctr.cluster(**ccckwargs)
-        # Populate metadata
+        Logger.info(f'POPULATING EVENT METADATA')
         ctr.populate_event_metadata()
-        # Write to disk
-        Logger.info(f'Writing to disk')
-        SAVENAME = RDIR / chanid
-        ctr.write(str(SAVENAME))
-        # Mark chanid as processed
-        processed_chanids.add(chanid)
+        Logger.info(f'WRITING TO DISK')
+        ctr.write(str(RDIR/name))
+        processed.add(name)
 
-    # breakpoint()
 
 
 if __name__ == '__main__':
     # Setup Logging
     Logger = setup_terminal_logger(os.path.split(__file__)[-1], level=logging.INFO)
     Logger.addHandler(CriticalExitHandler(exit_code=1))
-    # Set up to-file logger
-    # CLOG =
-
+    # RUN MAIN
     main()   
