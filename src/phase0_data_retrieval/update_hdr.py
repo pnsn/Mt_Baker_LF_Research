579cb6b55abdb5d5622ea1effc4eaec8c7f886d3
Nate Stevens, PhD <33462727+nts345045@users.noreply.github.com>
2025-07-25 16:15:45 -0700

    Add sac  options

--- src/phase0_data_retrieval/update_hdr.py
+++ src/phase0_data_retrieval/update_hdr.py
@@ -6,7 +6,7 @@ import pandas as pd
 from obspy import read
 
 
-def main():
+def main(format='SAC'):
     ROOT = Path(__file__).parent.parent.parent
     WWU = ROOT/'data'/'WWU'
     NET = 'WW'
@@ -15,18 +15,19 @@ def main():
 
     for YEAR in ['2007','2008','2009']:
         RAW_STA_DIRS = glob(str(WWU/'RAW'/YEAR/'*'))
-        breakpoint()
         for STA in [os.path.split(_e)[-1] for _e in RAW_STA_DIRS]:
             RAWZ = WWU/'RAW'/YEAR/STA/'*z*'/f'{YEAR}*.sac'
             RAWN = WWU/'RAW'/YEAR/STA/'*n*'/f'{YEAR}*.sac'
             RAWE = WWU/'RAW'/YEAR/STA/'*e*'/f'{YEAR}*.sac'
             DGPS = WWU/'DATA'/YEAR/STA/'GPS'/'parsed_clock_data.csv'
-            DATA = WWU/'DATA'/YEAR/STA/'SAC'
+            DATA = WWU/'DATA'/YEAR/STA/format
 
-            df = pd.read_csv(DGPS, index_col=[0], parse_dates=['utcdatetime'])
-            stla = df.lat.median()
-            stlo = df.lon.median()
-            stel = df.elev.median()
+
+            if format == 'SAC':
+                df = pd.read_csv(DGPS, index_col=[0], parse_dates=['utcdatetime'])
+                stla = df.lat.median()
+                stlo = df.lon.median()
+                stel = df.elev.median()
 
             try:
                 os.makedirs(str(DATA), exist_ok=False)
@@ -52,11 +53,12 @@ def main():
                     tr.stats.station = STA
                     tr.stats.location = LOC
                     tr.stats.channel = CBI+_C
-                    tr.stats.sac.update({'stla': stla, 'stlo': stlo, 'stle': stel})
-                    savename = f"{NET}.{STA}.{LOC}.{CBI}{_C}_{tr.stats.starttime.strftime('%Y%m%d_%H%M%S')}.sac"
+                    if format == 'SAC':
+                        tr.stats.sac.update({'stla': stla, 'stlo': stlo, 'stle': stel})
+                    savename = f"{NET}.{STA}.{LOC}.{CBI}{_C}_{tr.stats.starttime.strftime('%Y%m%d_%H%M%S')}.{format.lower()}"
                     print(f'Writing: {DATA/savename}')
-                    tr.write(str(DATA/savename), format='SAC')            
+                    tr.write(str(DATA/savename), format=format.upper())            
 
 
 if __name__ == '__main__':
-    main()
+    main(format='mseed')
